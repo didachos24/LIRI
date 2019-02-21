1499f12640ee754133d621ab9f66575fe0e47d8a
Diego <didachos24@gmail.com>
2019-02-20 23:03:18 -0600

    Fixed switch statement for order

--- LIRI.js
+++ LIRI.js
@@ -2,75 +2,68 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-// var order = process.argv[3];
+var order = process.argv[2];
 
-// --------------------------------------------------------------------------------------
+  switch(order) {
+  case "concert":
 
+var concert = require("axios");
+var moment = require("moment");
 
+var artist = process.argv[3];
 
-
-// --------------------------------------------------------------------------------------
-
-// switch(order) {
-//   case concert:
-
-// var concert = require("axios");
-// var moment = require("moment");
-
-// var artist = process.argv[3];
-
-// concert.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response) {
+concert.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response) {
   
-// var date = response.data[0].datetime;
-// date = moment(date).format('MM/DD/YYYY');
+var date = response.data[0].datetime;
+date = moment(date).format('MM/DD/YYYY');
 
-//   console.log("\n================ Concert =================\n\nVenue: "+response.data[0].venue.name+"\nLocation: "+response.data[0].venue.city+", "+response.data[0].venue.region+", "+response.data[0].venue.country+"\nDate: "+date);
+  console.log("\n================ Concert =================\n\nVenue: "+response.data[0].venue.name+"\nLocation: "+response.data[0].venue.city+", "+response.data[0].venue.region+", "+response.data[0].venue.country+"\nDate: "+date);
+});
 
-// });
+  break;
+  case "spotify":
 
-//   break;
+var Spotify = require('node-spotify-api');
 
-//   case spotify:
+var spotify = new Spotify(keys.spotify);
 
-// var Spotify = require('node-spotify-api');
+var song = process.argv[3];
 
-// var spotify = new Spotify(keys.spotify);
+if(!song) {
+  song = "The sign";
+  artist = "Ace of Base";
+}
 
-// var song = process.argv[3];
+spotify.search({ type: 'track', query: song, limit: '1'}, function(err, data) { if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+    console.log("\n================ Song =================\n\nArtist(s): "+data.tracks.items[0].artists[0].name+"\nSong's name: "+data.tracks.items[0].name+"\nLink from Spotify: "+data.tracks.items[0].external_urls.spotify+"\nAlbum: "+data.tracks.items[0].album.name);
 
-// if(!song) {
-//   song = "The sign";
-//   artist = "Ace of Base";
-// }
+});
 
-// spotify.search({ type: 'track', query: song, limit: '1'}, function(err, data) { if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-//     console.log("\n================ Song =================\n\nArtist(s): "+data.tracks.items[0].artists[0].name+"\nSong's name: "+data.tracks.items[0].name+"\nLink from Spotify: "+data.tracks.items[0].external_urls.spotify+"\nAlbum: "+data.tracks.items[0].album.name);
+  break;
+  case "movie":
 
-// });
+var movie = require("axios");
 
-// break;
+var title = process.argv[3];
 
-//  case movie:
+if(title == null) {
+  title = "Mr.+Nobody";
+}
 
-// var movie = require("axios");
+movie.get("http://www.omdbapi.com/?t="+title+"&apikey=trilogy").then(
+function(response) {
+  console.log("\n================ Movie =================\n\nTitle: "+response.data.Title+"\nYear: "+response.data.Year+"\nIMDB Rating: "+response.data.imdbRating+"\nCountry: "+response.data.Country+"\nLanguage: "+response.data.Language+"\nPlot: "+response.data.Plot+"\nActors: "+response.data.Actors);
 
-// var title = process.argv[3];
+})
 
-// if(title == null) {
-//   title = "Mr.+Nobody";
-// }
+  break;
+  case "doIt":
 
-// movie.get("http://www.omdbapi.com/?t="+title+"&apikey=trilogy").then(
-// function(response) {
-//   console.log("\n================ Movie =================\n\nTitle: "+response.data.Title+"\nYear: "+response.data.Year+"\nIMDB Rating: "+response.data.imdbRating+"\nCountry: "+response.data.Country+"\nLanguage: "+response.data.Language+"\nPlot: "+response.data.Plot+"\nActors: "+response.data.Actors);
+var fs = require("fs");
 
-// })
+var ran = random.txt.readFile;
 
-// break;
-
-// case "do-what-it-says":
-
-// }
-
+console.log(ran);
+}
